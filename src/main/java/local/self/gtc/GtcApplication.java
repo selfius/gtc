800c96f13696d34515b33437b82e0a3509ee8ff6
Andrei Sviridov <self.polaris@gmail.com>
2018-07-14 13:52:10 +0200

    initial commit with app skeleton

---
+++ src/main/java/local/self/gtc/GtcApplication.java
@@ -0,0 +1,12 @@
+package local.self.gtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtcApplication.class, args);
+    }
+}
