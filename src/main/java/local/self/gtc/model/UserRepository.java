19c9d085668bb4a1218652a157e8e2d4e892fe39
Andrei Sviridov <self.polaris@gmail.com>
2018-07-14 14:28:10 +0200

    db setup

---
+++ src/main/java/local/self/gtc/model/UserRepository.java
@@ -0,0 +1,7 @@
+package local.self.gtc.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
--- src/test/java/local/self/gtc/GtcApplicationTests.java
+++ src/test/java/local/self/gtc/GtcApplicationTests.java
@@ -1,7 +1,10 @@
 package local.self.gtc;
 
+import local.self.gtc.model.User;
+import local.self.gtc.model.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GtcApplicationTests {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Test
     public void contextLoads() {
+        userRepository.save(new User("someName",123));
     }
 
 }
